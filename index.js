ceee2e35158ad05d10da41f30af071b6f1694be9
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-09-09 00:35:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ class Bird {
 
   speak() { 
     if  (this.gender = "male") { 
-    return ` It's me! ${this.name}, the parrot!`
+    return ` It's me! ${this.name}, the parrot!` 
+    } 
     else 
     return ` ${this.name} says squawk!`
   } 
